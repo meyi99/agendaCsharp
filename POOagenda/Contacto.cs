e051d9efd6d3dbf906fa84ea3bcac519f1b52afb
meyi <rubencastilloescamilla89@gmail.com>
2019-06-18 22:51:45 +0200

    first commit

---
+++ POOagenda/Contacto.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace POOagenda
+{
+    class Contacto
+    {
+        //Atributos
+        string nombre;
+        string apellido1;
+        string apellido2;
+        string direccion;
+        string telMovil;
+        string telFijo;
+        string mail;
+
+        //Constructor
+        public Contacto(string n, string ap1, string ap2, string tMvl)
+        {
+            nombre = n;
+            apellido1 = ap1;
+            apellido2 = ap2;
+            telMovil = tMvl;
+        }
+
+        //Constructor
+        public Contacto(string n, string ap1, string tMvl)
+        {
+            nombre = n;
+            apellido1 = ap1;
+            telMovil = tMvl;
+        }
+
+        //Constructor
+        public Contacto(string n, string ap1, string ap2, string tMvl, string dir, string tFijo, string mail)
+        {
+            nombre = n;
+            apellido1 = ap1;
+            apellido2 = ap2;
+            direccion = dir;
+            telMovil = tMvl;
+            telFijo = tFijo;
+            this.mail = mail; 
+        }
+
+        //Métodos propios
+        public void SetNombre(string n)
+        {
+            nombre = n;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public void SetApellido1(string ap1)
+        {
+            apellido1 = ap1;
+        }
+
+        public string GetApellido1()
+        {
+            return apellido1;
+        }
+
+        public void SetApellido2(string ap2)
+        {
+            apellido2 = ap2;
+        }
+
+        public string GetApellido2()
+        {
+            return apellido2;
+        }
+
+        public void SetTelMovil(string tMvl)
+        {
+            telMovil = tMvl;
+        }
+
+        public string GetTelMovil()
+        {
+            return telMovil;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ POOagenda/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace POOagenda
+{
+    class MainClass
+    {
+        // Función Menu()
+        static int Menu()
+        {
+            int opcion = 0;
+            DateTime date = new DateTime();
+            date = DateTime.Now;
+            Console.WriteLine("FECHA\t{0}", date);
+            Console.WriteLine("\n----------------------------");
+            Console.WriteLine("1. Insertar contacto");
+            Console.WriteLine("2. Visualizar agenda");
+            Console.WriteLine("3. Salir");
+            Console.WriteLine("Escoge una opción");
+            opcion = int.Parse(Console.ReadLine());
+
+            return opcion;
+        }
+
+        // Función NuevoContacto()
+        static Contacto NuevoContacto()
+        {
+            string nombre, ap1, ap2, telMovil;
+            Console.WriteLine("Introduce el nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Introduce el primer apellido");
+            ap1 = Console.ReadLine();
+            Console.WriteLine("Introduce el segundo apellido");
+            ap2 = Console.ReadLine();
+            Console.WriteLine("Introduce el teléfono móvil");
+            telMovil = Console.ReadLine();
+            Contacto nuevo = new Contacto(nombre, ap1, ap2, telMovil);
+
+            return nuevo;
+        }
+
+        // Función InsertarContacto()
+        static void InsertarContacto(Contacto[] ag, ref int nTotal, Contacto nuevo)
+        {
+            ag[nTotal] = nuevo; // nuevo es un contacto ya creado
+            nTotal++;
+        }
+
+        // Función Mostrar()
+        static void Mostar(Contacto[] ag, int nTotal)
+        {
+            for(int i = 0; i < nTotal; i++)
+            {
+                Console.WriteLine("Nombre\t\tApellido1\t\tApellido2\t\tTeléfono Móvil");
+                Console.WriteLine("------------------------------------------------------------------------------\n");
+                Console.WriteLine("{0}\t\t{1}\t\t{2}\t\t{3}", ag[i].GetNombre(), ag[i].GetApellido1(), ag[i].GetApellido2(), ag[i].GetTelMovil());
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            const int TAM_MAXIMO = 100;
+            int numElementos = 0;
+            int opcion = 0;
+
+            // Creación del array de objetos
+            Contacto[] agenda = new Contacto[TAM_MAXIMO];
+            do
+            {
+                Console.Clear();
+                opcion = Menu();
+                switch (opcion)
+                {
+                    case 1:
+                        Contacto n = NuevoContacto(); // Creación de un nuevo contacto
+                        InsertarContacto(agenda, ref numElementos, n);
+                        break;
+
+                    case 2:
+                        Mostar(agenda, numElementos);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("El programa ha finalizado.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción escogida incorrecta.");
+                        break;
+                }
+                Console.ReadLine();
+            } while (opcion != 3);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
